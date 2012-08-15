0b3d1f5de08ff00c87930df4be1fd2af6794c0f8
Rob Spectre <rob.spectre@gmail.com>
2012-08-15 10:35:00 -0400

    First pass at demo.

--- app.py
+++ app.py
@@ -1,70 +1,44 @@
-import os
-
-from flask import Flask
-from flask import render_template
-from flask import url_for
+import flask
 from flask import request
-
 from twilio import twiml
 from twilio.util import TwilioCapability
+import os
 
 
-# Declare and configure application
-app = Flask(__name__, static_url_path='/static')
-app.config.from_pyfile('local_settings.py')
+app = flask.Flask(__name__)
 
 
-# Voice Request URL
-@app.route('/voice', methods=['GET', 'POST'])
-def voice():
+@app.route('/queue', methods=['POST'])
+def queue():
     response = twiml.Response()
-    response.say("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.enqueue("GA Demo Night", wait_url="/wait")
     return str(response)
 
-
-# SMS Request URL
-@app.route('/sms', methods=['GET', 'POST'])
-def sms():
+@app.route('/wait', methods=['POST'])
+def wait():
     response = twiml.Response()
-    response.sms("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.say("You are number %s in line.  Please hold." %
+            request.form['QueuePosition'])
+    response.play("http://demo.brooklynhacker.com/music/ramones.mp3")
     return str(response)
 
+@app.route('/dial', methods=['POST'])
+def dial():
+    response = twiml.Response()
+    with response.dial() as dial:
+        dial.queue("GA Demo Night")
+    return str(response)
 
-# Twilio Client demo template
 @app.route('/client')
 def client():
-    configuration_error = None
-    for key in ('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_APP_SID',
-            'TWILIO_CALLER_ID'):
-        if not app.config[key]:
-            configuration_error = "Missing from local_settings.py: " \
-                    "%s" % key
-            token = None
-    if not configuration_error:
-        capability = TwilioCapability(app.config['TWILIO_ACCOUNT_SID'],
-            app.config['TWILIO_AUTH_TOKEN'])
-        capability.allow_client_incoming("joey_ramone")
-        capability.allow_client_outgoing(app.config['TWILIO_APP_SID'])
-        token = capability.generate()
-    return render_template('client.html', token=token,
-            configuration_error=configuration_error)
+    capability = TwilioCapability(os.environ.get('TWILIO_ACCOUNT_SID'),
+            os.environ.get('TWILIO_AUTH_TOKEN'))
+    capability.allow_client_outgoing("AP0a7346ba3b7e17441da9b792d76f149e")
+    token = capability.generate()
+    return flask.render_template("client.html", token=token)
 
 
-# Installation success page
-@app.route('/')
-def index():
-    params = {
-        'voice_request_url': url_for('.voice', _external=True),
-        'sms_request_url': url_for('.sms', _external=True),
-        'client_url': url_for('.client', _external=True)}
-    return render_template('index.html', params=params)
-
-
-# If PORT not specified by environment, assume development config.
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    if port == 5000:
-        app.debug = True
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
